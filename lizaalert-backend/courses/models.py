ded3247c7c3981b28b5d2753354f6968983151e7
Roman Rogachev <75900827+Dozo-org@users.noreply.github.com>
2022-04-01 19:42:10 +0300

    LA-#20. Add base model: Course
    * Add base model: Course
    
    * Remove 'attempts_limit' in Course model
    
    * Edited the migrations file for the Cource model

--- lizaalert-backend/courses/models.py
+++ lizaalert-backend/courses/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Course(models.Model):
+    title = models.CharField(
+        max_length=120,
+        verbose_name='Название курса'
+    )
+    format = models.CharField(
+        max_length=60,
+        verbose_name='Формат курса'
+    )
+    start_date = models.DateField(
+        blank=True,
+        null=True,
+        verbose_name='Дата начала курса'
+    )
+    cover_path = models.FileField(
+        blank=True,
+        null=True,
+        verbose_name='Путь к обложке курса'
+    )
+    short_description = models.CharField(
+        max_length=120,
+        verbose_name='Краткое описание курса'
+    )
+    full_description = models.TextField(
+        verbose_name='Полное описание курса'
+    )
+    user_created = models.ForeignKey(
+        User,
+        on_delete=models.PROTECT,
+        verbose_name='Создатель курса'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания курса'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата изменения курса'
+    )
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.title
