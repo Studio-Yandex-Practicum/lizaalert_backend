ef6adcab92e7dd95ea0dffc57c688f59ee666351
yauheni <smilodon2@yandex.ru>
2022-05-20 23:07:12 +0300

    вместо числового значения статусов, использую HTTP.status добавил классовую переменную url в которую получаю url адрес для тестирования

--- lizaalert-backend/tests/tests/test_user.py
+++ lizaalert-backend/tests/tests/test_user.py
@@ -1,41 +1,45 @@
 import pytest
+from django.urls import reverse
+from rest_framework import status
 
 statuses = [
-  {
-    "id": 1,
-    "name": "Активный",
-    "slug": "active"
-  },
-  {
-    "id": 2,
-    "name": "Вы записаны",
-    "slug": "booked"
-  },
-  {
-    "id": 3,
-    "name": "Пройден",
-    "slug": "finished"
-  },
-  {
-    "id": 4,
-    "name": "Не активный",
-    "slug": "inactive"
-  }
+    {
+        "id": 1,
+        "name": "Активный",
+        "slug": "active"
+    },
+    {
+        "id": 2,
+        "name": "Вы записаны",
+        "slug": "booked"
+    },
+    {
+        "id": 3,
+        "name": "Пройден",
+        "slug": "finished"
+    },
+    {
+        "id": 4,
+        "name": "Не активный",
+        "slug": "inactive"
+    }
 ]
 
+
 class TestCourseStatus:
+    url = reverse('users-list')
 
     def test_coursestatus_list_not_found(self, user_client):
-        response = user_client.get(f'/api/v1/users/course_statuses/')
 
-        assert response.status_code != 404
+        response = user_client.get(self.url)
+        assert response.status_code != status.HTTP_404_NOT_FOUND
 
     def test_coursestatus_list_anonimous(self, client):
-        response = client.get(f'/api/v1/users/course_statuses/')
-        assert response.status_code == 401
+        response = client.get(self.url)
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_coursestatus_list(self, user_client, create_statuses):
-        response = user_client.get(f'/api/v1/users/course_statuses/')
+        response = user_client.get(self.url)
 
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         assert response.json() == statuses
