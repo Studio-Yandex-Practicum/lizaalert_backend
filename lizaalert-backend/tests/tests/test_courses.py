5e7877e2c49dd27bb5160ceb10026f9253046b80
yauheni <smilodon2@yandex.ru>
2022-05-23 23:26:19 +0300

    убрал информацию о статусах курса

--- lizaalert-backend/tests/tests/test_courses.py
+++ lizaalert-backend/tests/tests/test_courses.py
@@ -2,44 +2,23 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
-statuses = [
-    {
-        "id": 1,
-        "name": "Активный",
-        "slug": "active"
-    },
-    {
-        "id": 2,
-        "name": "Вы записаны",
-        "slug": "booked"
-    },
-    {
-        "id": 3,
-        "name": "Пройден",
-        "slug": "finished"
-    },
-    {
-        "id": 4,
-        "name": "Не активный",
-        "slug": "inactive"
-    }
-]
-
 
 class TestCourseStatus:
-    url = reverse('users-list')
+    url = reverse('courses_statuses-list')
 
+    @pytest.mark.django_db(transaction=True)
     def test_coursestatus_list_not_found(self, user_client):
-
         response = user_client.get(self.url)
         assert response.status_code != status.HTTP_404_NOT_FOUND
 
+    @pytest.mark.django_db(transaction=True)
     def test_coursestatus_list_anonimous(self, client):
         response = client.get(self.url)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
+    @pytest.mark.django_db(transaction=True)
     def test_coursestatus_list(self, user_client, create_statuses):
         response = user_client.get(self.url)
 
         assert response.status_code == status.HTTP_200_OK
-        assert response.json() == statuses
+        assert response.json() == create_statuses
