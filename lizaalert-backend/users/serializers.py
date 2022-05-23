1d96e8a020900610b0bf07056a9a0445a29e9929
yauheni <smilodon2@yandex.ru>
2022-05-23 22:51:12 +0300

    убрал все относящееся к course-statuses в приложение courses

--- lizaalert-backend/users/serializers.py
+++ lizaalert-backend/users/serializers.py
@@ -1,9 +1,2 @@
 from rest_framework import serializers
 
-from .models import CourseStatus
-
-
-class CourseStatusSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CourseStatus
-        fields = "__all__"
--- lizaalert-backend/users/urls.py
+++ lizaalert-backend/users/urls.py
@@ -1,11 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from .views import CourseStatusViewSet
 
-router = DefaultRouter()
-router.register(r"course_statuses", CourseStatusViewSet, basename="users")
 
-urlpatterns = [
-    path("users/", include(router.urls)),
-]
+
