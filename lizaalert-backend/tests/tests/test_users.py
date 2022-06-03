bd602ea5ab0bb3d5f996302aac218971d143a6c6
yauheni <smilodon2@yandex.ru>
2022-06-04 00:29:35 +0300

    добавил класс TestLevel c тестами эндпойнтов для получия списка уровней

---
+++ lizaalert-backend/tests/tests/test_users.py
@@ -0,0 +1,19 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+
+class TestLevel:
+    url = reverse('level-list')
+
+    @pytest.mark.django_db(transaction=True)
+    def test_level_list_not_found(self, user_client):
+        response = user_client.get(self.url)
+        assert response.status_code != status.HTTP_404_NOT_FOUND
+
+    @pytest.mark.django_db(transaction=True)
+    def test_level_list(self, user_client, create_levels):
+        response = user_client.get(self.url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == create_levels
