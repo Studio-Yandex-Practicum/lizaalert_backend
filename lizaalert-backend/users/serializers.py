15d1d53fbb8fde0f1888076e8382ef11ae5550da
yauheni <smilodon2@yandex.ru>
2022-06-04 00:27:05 +0300

    добавил сериализатор LevelSeiralizer

--- lizaalert-backend/users/serializers.py
+++ lizaalert-backend/users/serializers.py
@@ -1,2 +1,14 @@
 from rest_framework import serializers
 
+from .models import Level
+
+
+class LevelSerializer(serializers.ModelSerializer):
+    slug = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Level
+        exclude = ('description', )
+
+    def get_slug(self, obj):
+        return obj.get_name_display()
