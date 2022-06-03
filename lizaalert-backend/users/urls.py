e2f34bd66703bcdfc0773c342ce68d75664a411c
yauheni <smilodon2@yandex.ru>
2022-06-04 00:28:47 +0300

    добавил и зарегистрировал url для получения данных об уровнях

--- lizaalert-backend/users/urls.py
+++ lizaalert-backend/users/urls.py
@@ -1,6 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
+from .views import LevelViewSet
 
+router = DefaultRouter()
+router.register(r"level", LevelViewSet, basename="level")
 
+urlpatterns = [
+    path("", include(router.urls)),
+]
 
