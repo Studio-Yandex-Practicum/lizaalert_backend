3520167087b6accb683f3e21b705a123b1d81925
yauheni <smilodon2@yandex.ru>
2022-06-04 00:46:00 +0300

    добавил файл с фикстурой уровня

---
+++ lizaalert-backend/tests/tests/user_fixtures/level_fixtures.py
@@ -0,0 +1,27 @@
+import pytest
+
+levels = [
+    {
+        "id": 1,
+        "name": "Новичок",
+        "slug": "novice"
+    },
+    {
+        "id": 2,
+        "name": "Бывалый",
+        "slug": "experienced"
+    },
+    {
+        "id": 3,
+        "name": "Профессионал",
+        "slug": "professional"
+    }
+]
+
+
+@pytest.fixture
+def create_levels():
+    from users.models import Level
+    [Level.objects.create(name=level["name"], description='test') for level in levels]
+    out = [{"id": o.id, "name": o.name, "slug": o.get_name_display()} for o in Level.objects.all()]
+    return out
