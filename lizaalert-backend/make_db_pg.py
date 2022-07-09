ee7081d8d19fb19e198ec29439810bfdda69709f
Sergey Konenkov <82638131+Katsmannn@users.noreply.github.com>
2022-07-09 06:56:35 +0500

    Update make_db_pg.py
    Удалён лишний комментарий

--- lizaalert-backend/make_db_pg.py
+++ lizaalert-backend/make_db_pg.py
@@ -59,7 +59,6 @@ def schema_migration():
     # migrate
     call_command("migrate")
     # add superuser
-    # from django.contrib.auth.models import User
     from django.contrib.auth import get_user_model
 
     User = get_user_model()
