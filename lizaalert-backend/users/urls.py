83dbe0c67d0999af4b718ef38f0362dc628d0a10
wrawka <wrawka@gmail.com>
2022-07-11 15:56:40 +0300

    small fixes to make linters happy

--- lizaalert-backend/users/urls.py
+++ lizaalert-backend/users/urls.py
@@ -1,6 +1,2 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter, SimpleRouter
-
-
-
-
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter, SimpleRouter
