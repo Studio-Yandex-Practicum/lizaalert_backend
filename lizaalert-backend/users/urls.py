7c05bc32f0706842aa7ee3a71f83297295203842
wrawka <wrawka@gmail.com>
2022-07-16 17:26:12 +0300

    added route for `roles`

--- lizaalert-backend/users/urls.py
+++ lizaalert-backend/users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import LevelViewSet
+from .views import LevelViewSet, ListRoles
 
 router = DefaultRouter()
 router.register(r"level", LevelViewSet, basename="level")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("roles/", ListRoles.as_view())
 ]
--- lizaalert-backend/users/views.py
+++ lizaalert-backend/users/views.py
@@ -1,9 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import views, viewsets
+from rest_framework.response import Response
 
-from .models import Level
+from .models import Level, UserRole
 from .serializers import LevelSerializer
 
 
 class LevelViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Level.objects.all()
     serializer_class = LevelSerializer
+
+
+class ListRoles(views.APIView):
+    def get(self, request):
+        results = [{"name": name, "description": description} for name, description in UserRole.Role.choices]
+        return Response({"results": results})
