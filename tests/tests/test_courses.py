9cf704583a143f742b309d30c5c78e40caa5845f
yauheni <smilodon2@yandex.ru>
2022-08-03 16:03:37 +0300

    добавил тесты доступности эндпойнта добавил тест подсчета кол-ва уроков в курсе и длительность уроков в курсе добавил проверку статуса курсов для анонимного пользователя добавил проверку статуса курсов

--- tests/tests/test_courses.py
+++ tests/tests/test_courses.py
@@ -24,3 +24,30 @@ class TestCourseStatusAndLevel:
         response = user_client.get(url)
         assert response.status_code == status.HTTP_200_OK
         assert response.json() == test_data()
+
+
+@pytest.mark.django_db(transaction=True)
+class TestCourse:
+    url = reverse("courses-list")
+
+    def test_not_found_courselist(self, user_client, create_chapter):
+        response = user_client.get(self.url)
+        assert response.status_code != status.HTTP_404_NOT_FOUND
+
+    def test_count_lessons_count_duration(self, user_client, create_chapter):
+        response = user_client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()['results'][0]['lessons_count'] == 2
+        assert response.json()['results'][0]['course_duration'] == 3
+
+    def test_course_status_anonymous(self, anonymous_client, create_chapter, create_volunteer_course):
+        response = anonymous_client.get(self.url)
+        courses = response.json()['results']
+        course_status = [course['course_status'] == 'inactive' for course in courses]
+        assert response.status_code == status.HTTP_200_OK
+        assert all(course_status)
+
+    def test_course_status_user(self, user_client, create_chapter, create_volunteer_course):
+        response = user_client.get(self.url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json()['results'][0]['course_status'] == 'active'
