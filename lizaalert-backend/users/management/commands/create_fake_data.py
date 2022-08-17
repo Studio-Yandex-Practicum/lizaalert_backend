bbcefa2dd51316467407ab3315dd6a725f9269cd
yauheni <smilodon2@yandex.ru>
2022-08-17 23:48:17 +0300

    Команда генерирует 5 курсов из 1 Части и 3х уроков в составе части. Уроки имеют статус Ready. Перед созданием курсов проверяется что в БД уже создано не менее 10 пользователей и есть созданные уровни.

---
+++ lizaalert-backend/users/management/commands/create_fake_data.py
@@ -0,0 +1,28 @@
+from courses.course_factory import LessonWith3ChapterFactory
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+from users.models import Level
+from users.user_factory import UserFactory, LevelFactory
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Generate fake course data'
+
+    def __generate_records(self, record_number, object_name):
+        for _ in range(record_number):
+            object_name()
+
+    def handle(self, *args, **options):
+        course_num = options['courses']
+        if User.objects.all().count() < 10:
+            self.__generate_records(course_num, UserFactory)
+        if not Level.objects.all().exists():
+            self.__generate_records(course_num, LevelFactory)
+        for _ in range(course_num):
+            LessonWith3ChapterFactory()
+
+    def add_arguments(self, parser):
+        parser.add_argument('--courses', type=int, default=5,
+                            help='Number of records generated with script')
