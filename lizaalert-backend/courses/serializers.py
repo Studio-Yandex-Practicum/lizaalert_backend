3a16ca4a3f906aaf610588d862cca7f88b2da1d5
yauheni <smilodon2@yandex.ru>
2022-08-24 00:57:26 +0300

    Вынес общие поля в отдельный миксин класс Добавил сериализаторы части(главы) и урока для отображения на странице курса Добавил

--- lizaalert-backend/courses/serializers.py
+++ lizaalert-backend/courses/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from .models import Course, CourseStatus
+from .models import Course, CourseStatus, Chapter, Lesson
+
+
+class CourseCommonFieldsMixin(serializers.ModelSerializer):
+    level = serializers.StringRelatedField(read_only=True)
+    lessons_count = serializers.IntegerField()
+    course_duration = serializers.IntegerField()
+    course_status = serializers.StringRelatedField(read_only=True)
 
 
 class CourseStatusSerializer(serializers.ModelSerializer):
@@ -9,15 +16,37 @@ class CourseStatusSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    level = serializers.StringRelatedField(read_only=True)
-    lessons_count = serializers.IntegerField()
-    course_duration = serializers.IntegerField()
-    course_status = serializers.StringRelatedField(read_only=True)
-
+class CourseSerializer(CourseCommonFieldsMixin):
     class Meta:
         model = Course
         fields = ('id', 'title', 'level', 'short_description', 'lessons_count', 'course_duration',
                   'course_status', 'cover_path'
                   )
 
+
+class LessonInlineSerializer(serializers.ModelSerializer):
+    """Сериалайзер класс для вложенного списка уроков курса"""
+
+    class Meta:
+        model = Lesson
+        fields = ('lesson_type', 'lesson_status',)
+
+
+class ChapterInlineSerializer(serializers.ModelSerializer):
+    """Сериалайзер класс для вложенного списка частей курса"""
+    lessons = LessonInlineSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Chapter
+        fields = ('title', 'lessons')
+
+
+class CourseDetailSerializer(CourseCommonFieldsMixin):
+    chapters = ChapterInlineSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Course
+        fields = (
+            'id', 'title', 'level', 'full_description', 'start_date', 'cover_path', 'lessons_count',
+            'course_duration', 'chapters',
+        )
