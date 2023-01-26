c0da90f472e4b6668b5a49b9f167f94ffdded11d
YauheniKr <smilodon2@yandex.ru>
2023-01-26 19:46:23 +0300

    feature#61/coutses_lessons_list (#81)
    * добавил генерацию фэйковых данных для поля knowledge
    
    * добавил url для вывода списка уроков курса
    
    * добавил сериализатор для вывода списка уроков курса
    
    * добавил viewset для вывода списка уроков курса
    
    * поправлено форматирование с black
    
    * добавил новые модули drf-nested-routers и factory-boy с зависимостями
    
    * удалил неиспользуемые импорты
    
    * поправил форматирование docstring для JSONFactory
    
    * поправил импорты
    
    Co-authored-by: yauheni <smilodon2@mail.ru>

--- lizaalert-backend/courses/serializers.py
+++ lizaalert-backend/courses/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Chapter, ChapterLesson, Course, CourseStatus
+from .models import Chapter, ChapterLesson, Course, CourseStatus, Lesson
 
 
 class CourseCommonFieldsMixin(serializers.ModelSerializer):
@@ -83,4 +83,19 @@ class CourseDetailSerializer(CourseCommonFieldsMixin):
             "course_duration",
             "chapters",
             "knowledge",
+        )
+
+
+class CourseLessonListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = (
+            "id",
+            "title",
+            "description",
+            "lesson_type",
+            "tags",
+            "duration",
+            "additional",
+            "diploma",
         )
