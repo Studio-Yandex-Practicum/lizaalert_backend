9afdfeb99f2bfb86d59fbf5e597dcd2f48731b38
serg <3d_konseral@mail.ru>
2023-01-30 21:20:52 +0500

    Make migrations + ALLOWED_HOSTS

--- lizaalert-backend/settings/settings.py
+++ lizaalert-backend/settings/settings.py
@@ -16,6 +16,7 @@ DEBUG = env.bool("DEBUG", False)
 ALLOWED_HOSTS = env.list(
     "ALLOWED_HOSTS",
     [
+        "51.250.31.200",
         "51.250.35.214",
         "0.0.0.0",
         "127.0.0.1",
