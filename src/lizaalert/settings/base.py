fab783891c434936b3dc61408d35209f98618b8f
Di0niz <moskovchenkody@gmail.com>
2023-03-18 09:55:25 +0300

    move settings

--- src/lizaalert/settings/base.py
+++ src/lizaalert/settings/base.py
@@ -36,10 +36,6 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:80",
 ]
 
-# Setup support for proxy headers
-USE_X_FORWARDED_HOST = True
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
--- src/lizaalert/settings/production.py
+++ src/lizaalert/settings/production.py
@@ -1,1 +1,6 @@
 from lizaalert.settings.base import *  # noqa
+
+
+# Setup support for proxy headers
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
