430987936617e3e669f6df84e5539d2cda4c11ff
sniki-ld <dennisalenaf@gmail.com>
2023-03-22 19:02:32 +0300

    fix

--- src/lizaalert/courses/filters.py
+++ src/lizaalert/courses/filters.py
@@ -7,16 +7,13 @@ from .models import Course
 class CourseFilter(filters.FilterSet):
     """Кастомный фильтр для модели Course."""
 
-    format = django_filters.CharFilter(
-        field_name="course_format",
-        lookup_expr="icontains"
-    )
+    format = django_filters.CharFilter(field_name="course_format", lookup_expr="icontains")
 
-    level = django_filters.CharFilter(
-        field_name="level__name",
-        lookup_expr="icontains"
-    )
+    level = django_filters.CharFilter(field_name="level__name", lookup_expr="icontains")
 
     class Meta:
         model = Course
-        fields = ("level", "course_format",)
+        fields = (
+            "level",
+            "course_format",
+        )
