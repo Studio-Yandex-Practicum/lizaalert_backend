39e4371f62fe5377576c192e26411161becba61c
Ivan Golenko <igolivanenko1996@gmail.com>
2023-04-05 17:07:19 +0300

    92_the_student_can_open

--- src/lizaalert/courses/serializers.py
+++ src/lizaalert/courses/serializers.py
@@ -104,14 +104,10 @@ class CourseLessonListSerializer(serializers.ModelSerializer):
 
 
 class LessonDetailSerializer(serializers.ModelSerializer):
-    """
-    Сериалайзер класс для отображения деталей урока, в том числе:
-    next_lesson и prev_lesson.
-    """
+    """Сериалайзер класс для деталей урока, также next_lesson и prev_lesson."""
 
     next_lesson = serializers.SerializerMethodField()
     prev_lesson = serializers.SerializerMethodField()
-
 
     class Meta:
         model = Lesson
