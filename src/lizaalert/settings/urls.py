8941d1a029f1b3d22114038c9e243b4a00655b03
Артём <joker9099@mail.ru>
2023-08-02 18:33:08 +0300

    Revert "Feature/user authentication (#118)"
    This reverts commit a50c79cc6fd4d600e7f09c278c2f17893cb807f0.

--- src/lizaalert/settings/urls.py
+++ src/lizaalert/settings/urls.py
@@ -11,8 +11,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("lizaalert.courses.urls")),
     path("api/v1/", include("lizaalert.users.urls")),
-    path("api/v1/auth/", include("djoser.urls")),
-    path("api/v1/auth/", include("djoser.urls.jwt")),
+    path("auth/", include("lizaalert.authentication.urls")),
 ]
 
 if settings.DEBUG:
