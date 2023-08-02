49992d4306b67c7b25b6b9229b90a29f113b714b
Di0niz <moskovchenkody@gmail.com>
2023-08-02 09:23:10 +0300

    Update cors (#116)
    * update cors and tests
    
    * update command

--- src/lizaalert/settings/production.py
+++ src/lizaalert/settings/production.py
@@ -3,3 +3,19 @@ from lizaalert.settings.base import *  # noqa
 # Setup support for proxy headers
 USE_X_FORWARDED_HOST = True
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+CORS_ALLOWED_ORIGINS = [
+    "https://swagger.la-testing.ru",
+    "https://docs.la-testing.ru",
+    "http://localhost:3000",
+    "http://localhost:80",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "api.la-testing.ru",
+    "admin.la-testing.ru",
+    "swagger.la-testing.ru",
+    "docs.la-testing.ru",
+    "localhost:3000",
+    "localhost:80",
+]
