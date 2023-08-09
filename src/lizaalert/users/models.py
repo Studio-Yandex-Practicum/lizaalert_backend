282f23d63ed45a5131faf4aded4acfad21187a70
Артём <joker9099@mail.ru>
2023-08-09 22:00:46 +0300

    Fix User model

--- src/lizaalert/users/models.py
+++ src/lizaalert/users/models.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 from easy_thumbnails.fields import ThumbnailerImageField
