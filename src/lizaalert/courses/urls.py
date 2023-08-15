a469b382cff3f1b3eb7e98c4e7b8a4945d7fd745
sniki-ld <dennisalenaf@gmail.com>
2023-08-15 17:15:40 +0300

    Update urls.py

--- src/lizaalert/courses/urls.py
+++ src/lizaalert/courses/urls.py
@@ -2,11 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested import routers
 
-from lizaalert.courses.views import CourseLessonListViewSet, CourseStatusViewSet, CourseViewSet, FilterListView
+from lizaalert.courses.views import CourseLessonListViewSet, CourseStatusViewSet, CourseViewSet, FilterListViewSet
 
 router = DefaultRouter()
 router.register(r"courses", CourseViewSet, basename="courses")
-router.register(r"courses_statuses", CourseStatusViewSet, basename="courses_statuses")
+router.register(r"courses_statuses", CourseStatusViewSet, basename="courses_statuses"),
+router.register("filters", FilterListViewSet, basename="filters-list")
 
 domains_router = routers.NestedSimpleRouter(router, r"courses", lookup="courses")
 domains_router.register(r"lessons", CourseLessonListViewSet, basename="course-chapters-list")
@@ -14,5 +15,4 @@ domains_router.register(r"lessons", CourseLessonListViewSet, basename="course-ch
 urlpatterns = [
     path("", include(router.urls)),
     path(r"", include(domains_router.urls)),
-    path("filters/", FilterListView.as_view(), name="filters-list"),
 ]
