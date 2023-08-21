39819cd66c87064869d26839f6b37678daf3b11d
sniki-ld <dennisalenaf@gmail.com>
2023-08-21 07:22:34 +0300

    Update user_fixtures.py
    change parameter "username" to "email" to create test user;

--- src/tests/user_fixtures/user_fixtures.py
+++ src/tests/user_fixtures/user_fixtures.py
@@ -7,12 +7,12 @@ from lizaalert.users.models import UserRole
 
 @pytest.fixture
 def user(django_user_model):
-    return django_user_model.objects.create_user(username="TestUser", password="1234567")
+    return django_user_model.objects.create_user(email="testuser@test.com", password="1234567")
 
 
 @pytest.fixture
 def user_2(django_user_model):
-    return django_user_model.objects.create_user(username="TestUser2", password="1234567")
+    return django_user_model.objects.create_user(email="testuser2@test.com", password="1234567")
 
 
 @pytest.fixture
