984930bdc6b213e7bc4fca74919714576309cd61
Артём <joker9099@mail.ru>
2023-08-31 23:49:59 +0300

    Fix admin.py

--- src/lizaalert/quizzes/admin.py
+++ src/lizaalert/quizzes/admin.py
@@ -5,19 +5,19 @@ from .models import Question, Quiz, UserAnswer
 
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "created_at", "deleted_at")
+    list_display = ("id", "title", "created_at", "deleted_at")
     search_fields = ("title",)
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ("title", "quiz", "question_type", "order_number", "created_at", "deleted_at")
+    list_display = ("id", "title", "quiz", "question_type", "order_number", "created_at", "deleted_at")
     list_filter = ("quiz",)
     search_fields = ("title",)
 
 
 @admin.register(UserAnswer)
 class UserAnswerAdmin(admin.ModelAdmin):
-    list_display = ("user", "quiz", "start_time", "date_completed")
+    list_display = ("id", "user", "quiz", "start_time", "date_completed")
     list_filter = ("user", "quiz")
     search_fields = ("user__username", "quiz__title")
