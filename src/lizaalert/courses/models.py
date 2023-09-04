69a336f07def615e6b5dfd1fdf596d7ecf1a26bc
Di0niz <moskovchenkody@gmail.com>
2023-09-04 21:07:27 +0300

    remove model

--- src/lizaalert/courses/models.py
+++ src/lizaalert/courses/models.py
@@ -8,24 +8,6 @@ from lizaalert.quizzes.models import Quiz
 User = get_user_model()
 
 
-class TimeStampedModel(models.Model):
-    """
-    Абстрактная модель времени создания или изменения данных.
-
-    created_at* - дата создания записи об уроке, автоматическое проставление
-    текущего времени
-    updated_at* - дата обновления записи об уроке, автоматическое проставление
-    текущего времени.
-    """
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        abstract = True
-
-
 class FAQ(TimeStampedModel):
     """
     Класс для хранения списка часто задаваемых вопросов.
