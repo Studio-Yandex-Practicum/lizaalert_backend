b8d509a23906a5d5d6bd5a3d5c6829b03aa12382
Nikita Assorov <115094006+cosmofactory@users.noreply.github.com>
2023-09-26 14:42:01 +0200

    lesson_status admin-controlled feature (#149)
    * lesson_status admin-controlled feature
    
    * isort fix
    
    * isort fix
    
    * black style fix
    
    * status_digits rework
    
    * user_lesson_progress field in serializer
    
    * admin controlled course
    
    ---------
    
    Co-authored-by: cosmofactory <srvnik@gmail.com>

--- src/lizaalert/courses/serializers.py
+++ src/lizaalert/courses/serializers.py
@@ -1,6 +1,16 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from lizaalert.courses.models import FAQ, Chapter, ChapterLesson, Course, CourseStatus, Knowledge, Lesson
+from lizaalert.courses.models import (
+    FAQ,
+    Chapter,
+    ChapterLesson,
+    Course,
+    CourseStatus,
+    Knowledge,
+    Lesson,
+    LessonProgressStatus,
+)
 
 
 class CourseCommonFieldsMixin(serializers.ModelSerializer):
@@ -38,7 +48,7 @@ class LessonInlineSerializer(serializers.ModelSerializer):
     """Сериалайзер класс для вложенного списка уроков курса."""
 
     lesson_type = serializers.ReadOnlyField(source="lesson.lesson_type")
-    lesson_status = serializers.ReadOnlyField(source="lesson.lesson_status")
+    lesson_progress = serializers.SerializerMethodField()
     duration = serializers.ReadOnlyField(source="lesson.duration")
     title = serializers.ReadOnlyField(source="lesson.title")
 
@@ -48,10 +58,19 @@ class LessonInlineSerializer(serializers.ModelSerializer):
             "id",
             "order_number",
             "lesson_type",
-            "lesson_status",
+            "lesson_progress",
             "duration",
             "title",
         )
+
+    def get_lesson_progress(self, obj):
+        try:
+            user = self.context.get("request").user
+            lesson = obj.lesson
+            progress = get_object_or_404(LessonProgressStatus, user=user, lesson=lesson)
+            return progress.userlessonprogress
+        except Exception:
+            return "0"
 
 
 class ChapterInlineSerializer(serializers.ModelSerializer):
