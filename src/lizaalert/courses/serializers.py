04415fa864289c011414e18e8fbc248b192c5cc8
cosmofactory <nikssor@yandex.ru>
2023-10-10 12:12:58 +0300

    black fix

--- src/lizaalert/courses/serializers.py
+++ src/lizaalert/courses/serializers.py
@@ -15,7 +15,6 @@ class FaqInlineSerializer(serializers.ModelSerializer):
             "author",
             "created_at",
             "updated_at",
-
         )
 
 
@@ -30,7 +29,6 @@ class KnowledgeInlineSerializer(serializers.ModelSerializer):
             "author",
             "created_at",
             "updated_at",
-
         )
 
 
