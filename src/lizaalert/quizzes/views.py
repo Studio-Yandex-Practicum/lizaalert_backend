24d35df8820cc0901018e98d39ef6270d6479e7c
Артём <joker9099@mail.ru>
2023-10-26 14:34:38 +0300

    Review

--- src/lizaalert/quizzes/views.py
+++ src/lizaalert/quizzes/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.utils import timezone
 from rest_framework import generics, status
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import APIException, ValidationError
 from rest_framework.response import Response
 
 from lizaalert.courses.models import Lesson
@@ -10,11 +10,26 @@ from lizaalert.quizzes.models import Question, Quiz, UserAnswer
 from lizaalert.quizzes.serializers import QuizWithQuestionsSerializer, UserAnswerSerializer
 from lizaalert.quizzes.utils import compare_answers
 
+# class ErrorMessages:
+#     TEST_NOT_STARTED = "Тест еще не начат."
+#     TIME_EXPIRED = "Время вышло. Вы не успели."
+#     COUNT_EXPIRED = "Закончилось количество попыток."
 
-class ErrorMessages:
-    TEST_NOT_STARTED = "Тест еще не начат."
-    TIME_EXPIRED = "Время вышло. Вы не успели."
-    COUNT_EXPIRED = "Закончилось количество попыток."
+
+class QuizException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+
+class TestNotStartedException(QuizException):
+    default_detail = "Тест еще не начат."
+
+
+class TimeExpiredException(QuizException):
+    default_detail = "Время вышло. Вы не успели."
+
+
+class CountExpiredException(QuizException):
+    default_detail = "Закончилось количество попыток."
 
 
 class QuizDetailView(generics.RetrieveAPIView):
@@ -49,8 +64,11 @@ class RunQuizView(generics.CreateAPIView):
         user_answer = UserAnswer.objects.filter(user=user, quiz=quiz).order_by("-id").first()
         retry_count = 0
         if user_answer:
-            if quiz.retries != 0 and user_answer.retry_count >= quiz.retries:
-                return Response({"message": ErrorMessages.COUNT_EXPIRED}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                if quiz.retries != 0 and user_answer.retry_count >= quiz.retries:
+                    raise CountExpiredException("Попытки закончились")
+            except CountExpiredException as e:
+                return Response({"message": e.detail}, status=e.status_code)
             retry_count = user_answer.retry_count = user_answer.retry_count + 1
         else:
             if quiz.retries != 0:
@@ -101,8 +119,11 @@ class QuizDetailAnswerView(generics.CreateAPIView, generics.RetrieveAPIView):
             solution_time = user_answer.end_date - user_answer.start_date
             solution_time_minutes = solution_time.total_seconds() / 60
 
-            if solution_time_minutes > quiz.duration_minutes:
-                return Response({"message": ErrorMessages.TIME_EXPIRED}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                if solution_time_minutes > quiz.duration_minutes:
+                    raise TimeExpiredException("Время истекло")
+            except TimeExpiredException as e:
+                return Response({"message": e.detail}, status=e.status_code)
 
             serializer = UserAnswerSerializer(user_answer, data={"answers": data}, partial=True)
             if serializer.is_valid():
@@ -111,8 +132,8 @@ class QuizDetailAnswerView(generics.CreateAPIView, generics.RetrieveAPIView):
                 user_answer.final_result = False
             else:
                 user_answer.final_result = True
-        except UserAnswer.DoesNotExist:
-            return Response({"message": ErrorMessages.TEST_NOT_STARTED}, status=status.HTTP_400_BAD_REQUEST)
+        except TestNotStartedException as e:
+            return Response({"message": e.detail}, status=e.status_code)
         serializer = UserAnswerSerializer(user_answer, data={"answers": data}, partial=True)
 
         if serializer.is_valid():
