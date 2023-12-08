0517e50c4fd6d72fc36b74fff57fcd83daf5f148
vikail <vmikail@yandex.ru>
2023-12-08 17:17:47 +0300

    add field remaining retries

--- src/lizaalert/quizzes/serializers.py
+++ src/lizaalert/quizzes/serializers.py
@@ -54,6 +54,8 @@ class QuizWithQuestionsSerializer(QuizSerializer):
 
 
 class UserAnswerSerializer(serializers.ModelSerializer):
+    remaining_retries = serializers.SerializerMethodField()
+
     class Meta:
         model = UserAnswer
         fields = "__all__"
@@ -74,3 +76,9 @@ class UserAnswerSerializer(serializers.ModelSerializer):
         user_answer = UserAnswer(**validated_data)
         user_answer.save()
         return user_answer
+
+    def get_remaining_retries(self, instance):
+        max_retries = instance.quiz.retries
+        current_retries = instance.retry_count
+        remaining_retries = max(0, max_retries - current_retries)
+        return remaining_retries
