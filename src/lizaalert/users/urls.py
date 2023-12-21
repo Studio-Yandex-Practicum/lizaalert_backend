e7dc98ff6b251898225d1e4a576205b32c77971a
vikail <vmikail@yandex.ru>
2023-12-21 21:04:44 +0300

    Profile badges list

--- src/lizaalert/users/urls.py
+++ src/lizaalert/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from lizaalert.users.views import LevelViewSet, ListRoles, UserRoleViewSet, VolunteerAPIview
+from lizaalert.users.views import LevelViewSet, ListRoles, UserRoleViewSet, VolunteerAPIview, VolunteerBadgeList
 
 router = DefaultRouter()
 router.register(r"level", LevelViewSet, basename="level")
@@ -9,6 +9,7 @@ router.register(r"users/(?P<user_id>\d+)/roles", UserRoleViewSet, basename="user
 
 urlpatterns = [
     path("profile/", VolunteerAPIview.as_view(), name="profile"),
+    path("profile/badges/", VolunteerBadgeList.as_view(), name="profile"),
     path("roles/", ListRoles.as_view()),
     path("", include(router.urls)),
 ]
