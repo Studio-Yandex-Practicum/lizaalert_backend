9f8b16dd4791e67d3adee20b24d16eff2cb9139d
vikail <vmikail@yandex.ru>
2024-01-21 21:00:27 +0300

    bages list and filter codestyle fix

--- src/lizaalert/users/views.py
+++ src/lizaalert/users/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from lizaalert.courses.models import CourseProgressStatus
+from lizaalert.courses.models import Course, CourseProgressStatus
 from lizaalert.users.models import Badge, Level, User, UserRole, Volunteer, VolunteerBadge
 from lizaalert.users.serializers import BadgeSerializer, LevelSerializer, UserRoleSerializer, VolunteerSerializer
 
@@ -103,16 +103,18 @@ class VolunteerBadgeListViewSet(viewsets.ReadOnlyModelViewSet):
             400: "Bad Request",
         }
     )
-
     def list(self, request):
-        filter_value = request.query_params.get('course', None)
+        filter_value = request.query_params.get("course", None)
         volunteer = get_object_or_404(Volunteer, user=request.user)
         if filter_value:
             queryset = Badge.objects.filter(
-                id__in=VolunteerBadge.objects.filter(volunteer=volunteer.id, 
-                                                     course__in=Course.objects.filter(title=filter_value).values("id")
-                                                     ).values("badge_id"))
+                id__in=VolunteerBadge.objects.filter(
+                    volunteer=volunteer.id, course__in=Course.objects.filter(title=filter_value).values("id")
+                ).values("badge_id")
+            )
         else:
-            queryset = Badge.objects.filter(id__in=VolunteerBadge.objects.filter(volunteer=volunteer.id).values("badge_id"))
+            queryset = Badge.objects.filter(
+                id__in=VolunteerBadge.objects.filter(volunteer=volunteer.id).values("badge_id")
+            )
         serializer = BadgeSerializer(queryset, many=True)
         return Response(serializer.data)
