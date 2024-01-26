3a259b327460bd8680400d2c35251f5bfd56214f
cosmofactory <nikssor@yandex.ru>
2024-01-26 10:26:22 +0100

    remove keys

--- src/lizaalert/settings/base.py
+++ src/lizaalert/settings/base.py
@@ -203,9 +203,7 @@ API_URL = env.str("API_URL", None)
 
 # Sentry logging and monitoring
 sentry_sdk.init(
-    dsn=env.str(
-        "SENTRY_KEY", "https://c8497dd5a59067b6d6db55c204f3e650@o4506588233990144.ingest.sentry.io/4506627284336640"
-    ),
+    dsn=env.str("SENTRY_KEY"),
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
 )
