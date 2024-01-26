f03b197a1425afd9a4101d125d1a2ca1e3452d79
vikail <vmikail@yandex.ru>
2024-01-26 20:41:22 +0300

    TM-33 swagger profile scheme add

--- src/lizaalert/users/views.py
+++ src/lizaalert/users/views.py
@@ -12,8 +12,21 @@ from lizaalert.users.serializers import BadgeSerializer, LevelSerializer, UserRo
 
 
 class VolunteerAPIview(APIView):
+    """
+    Endpoint для работы с профилем пользователя.
+
+    Методы:
+    - GET: Отображение профиля пользователя.
+    - PATCH: Внесение изменений в профиль пользователя.
+
+    """
+
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        operation_description="Отображает профиль пользователя",
+        responses={200: VolunteerSerializer()},
+    )
     def get(self, request):
         volunteer = get_object_or_404(Volunteer, user=request.user)
         queryset = Volunteer.objects.annotate(
@@ -33,6 +46,11 @@ class VolunteerAPIview(APIView):
             return Response(serializer.data[0])
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @swagger_auto_schema(
+        operation_description="Внесение изменений в профиль пользователя",
+        request_body=VolunteerSerializer,
+        responses={200: VolunteerSerializer()},
+    )
     def patch(self, request):
         volunteer = get_object_or_404(Volunteer, user=request.user)
         serializer = VolunteerSerializer(volunteer, data=request.data, partial=True)
