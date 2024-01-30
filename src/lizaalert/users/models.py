05a166130abac9c58a55800c2f76940f5190a538
Артём <joker9099@mail.ru>
2024-01-30 20:41:41 +0300

    Fix migration

--- src/lizaalert/users/models.py
+++ src/lizaalert/users/models.py
@@ -1,8 +1,10 @@
+import sys
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models.signals import post_migrate, post_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from easy_thumbnails.fields import ThumbnailerImageField
 from phonenumber_field.modelfields import PhoneNumberField
