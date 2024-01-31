819e312e5395c0b3d42b21b1dd4cea2be98d31cd
vikail <vmikail@yandex.ru>
2024-01-31 21:24:55 +0300

    delete unused import

--- src/lizaalert/users/views.py
+++ src/lizaalert/users/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count, OuterRef, Subquery
 from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import mixins, permissions, status, views, viewsets, exceptions
+from rest_framework import mixins, permissions, status, views, viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
