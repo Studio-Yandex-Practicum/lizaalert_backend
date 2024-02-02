40afa9a4cba3365f07a9a337f01a988fdf10182d
vmikail <vmikail@yandex.ru>
2024-02-02 15:30:29 +0300

    unused import fix

--- src/lizaalert/users/views.py
+++ src/lizaalert/users/views.py
@@ -4,7 +4,6 @@ from django.utils.decorators import method_decorator
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, permissions, status, views, viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
