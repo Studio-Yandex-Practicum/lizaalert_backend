0c86f10f7e4600c3aefd29b3df38f328e32e7683
Leontiev93 <107325873+Leontiev93@users.noreply.github.com>
2024-02-05 22:05:19 +0300

    MIL-37 Исправление бага /comlete (#316)
    * Исправление бага MIL-37 /comlete
    
    * Исправление бага MIL-37 /comlete ver.2
    
    * Исправление бага MIL-37 /comlete ver.3
    
    * Исправление бага MIL-37 /comlete ver.3
    
    * Исправление бага MIL-37 /comlete ver.4
    
    * Исправление бага MIL-37 /comlete ver.4
    
    * Исправление бага MIL-37 /comlete ver.4
    
    * Исправление бага MIL-37 /comlete ver.5
    
    * Исправление бага MIL-37 /comlete ver.6

--- src/lizaalert/courses/exceptions.py
+++ src/lizaalert/courses/exceptions.py
@@ -13,3 +13,9 @@ class SubscriptionDoesNotExist(PermissionDenied):
     """Исключение для подписки для данного пользователя на данный курс."""
 
     default_detail = "Subscription does not exist"
+
+
+class ProgressNotFinishedException(PermissionDenied):
+    """Исключение для завершения курса при незавершенных уроках."""
+
+    default_detail = "Имеются незавершенные уроки"
