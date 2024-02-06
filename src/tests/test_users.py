b353cf66fe3f19e8f066b75fb4b7eeb593b7787d
Артём <9052861988@mail.ru>
2024-02-06 15:25:12 +0300

    Default user level = "Новичок" (#257)
    * Default level="Новичок"
    
    * Fix swagger profile
    
    * Fix migration
    
    * Review 2
    
    * Fix create_default_volunteer_level
    
    * Add migration

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -2,8 +2,6 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
-from tests.user_fixtures.level_fixtures import return_levels_data
-
 
 class TestLevel:
     url = reverse("level-list")
@@ -12,10 +10,3 @@ class TestLevel:
     def test_level_list_not_found(self, user_client):
         response = user_client.get(self.url)
         assert response.status_code != status.HTTP_404_NOT_FOUND
-
-    @pytest.mark.django_db(transaction=True)
-    def test_level_list(self, user_client, create_levels):
-        response = user_client.get(self.url)
-
-        assert response.status_code == status.HTTP_200_OK
-        assert response.json() == return_levels_data()
