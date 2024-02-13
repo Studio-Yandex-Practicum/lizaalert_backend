dfa40914237a742b14cf13284f81bc9426ede20e
Герман Леонтьев <gerleonsto@gmail.com>
2024-02-13 21:58:36 +0300

    homework-url-views ver.1

--- src/lizaalert/homeworks/serializers.py
+++ src/lizaalert/homeworks/serializers.py
@@ -1,7 +1,5 @@
-from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
-from lizaalert.courses.models import Lesson, Subscription
 from lizaalert.homeworks.models import Homework
 
 
@@ -10,8 +8,9 @@ class HomeworkSerializer(serializers.ModelSerializer):
 
     status = serializers.ChoiceField(choices=Homework.ProgressionStatus)
     reviewer = serializers.StringRelatedField(read_only=True)
-    subscription = serializers.PrimaryKeyRelatedField(queryset=Subscription.objects.all())
-    #    lesson = serializers.PrimaryKeyRelatedField(queryset=Lesson.objects.all())
+    subscription = serializers.PrimaryKeyRelatedField(read_only=True)
+    lesson = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Homework
         fields = (
@@ -23,23 +22,6 @@ class HomeworkSerializer(serializers.ModelSerializer):
             "subscription",
             "required",
         )
-
-
-#     def create(self, validated_data):
-#         if 'id' not in self.initial_data:
-# #            homework = Homework.objects.create(**validated_data)
-
-#             return HomeworkSerializer(validated_data)
-#         else:
-#             print(1111)
-#             print('id' in self.initial_data)
-#             print(self.initial_data)
-#             print(validated_data)
-#             homework = Homework.objects.get(id=self.initial_data.get('id'))
-#             homework.text = self.initial_data.get('text')
-#             homework.status = Homework.ProgressionStatus.SUBMITTED
-#             homework.save
-#             return homework
 
 
 class HomeworkDetailSerializer(HomeworkSerializer):
--- src/lizaalert/homeworks/views.py
+++ src/lizaalert/homeworks/views.py
@@ -1,18 +1,12 @@
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
-from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
-from rest_framework.decorators import action
-from rest_framework.exceptions import APIException, ValidationError
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
-from lizaalert.courses.models import Course, Lesson, LessonProgressStatus, Subscription
+from lizaalert.courses.models import Course, Lesson, Subscription
 from lizaalert.homeworks.models import Homework
-from lizaalert.homeworks.serializers import HomeworkDetailSerializer, HomeworkSerializer
-
-# from lizaalert.users.models import Level
+from lizaalert.homeworks.serializers import HomeworkSerializer
 
 
 class HomeworkException(APIException):
@@ -27,7 +21,7 @@ class HomeworkViewSet(generics.ListAPIView):
     Класс дял отображение всех домашних работ одного преподавателя.
 
     Методы:
-    - GET: Получение информации о квизе и его вопросах.
+    - GET: Получение информации о домашних работах для преподавателя.
     """
 
     serializer_class = HomeworkSerializer
@@ -45,7 +39,7 @@ class HomeworkDetailViewSet(generics.RetrieveAPIView, generics.CreateAPIView):
 
     Методы:
     - GET: Получение информации домашней работе.
-    - POST:
+    - POST: При отправлении запроса необходимо передать {'text':'Текст'}
     """
 
     queryset = Homework.objects.all()
@@ -61,30 +55,27 @@ class HomeworkDetailViewSet(generics.RetrieveAPIView, generics.CreateAPIView):
             raise HomeworkException
 
     def create(self, request, *args, **kwargs):
-        lesson = request.data.get("lesson")
-        if lesson:
+        lesson = Lesson.objects.get(id=self.kwargs.get("lesson_id"))
+        homework = lesson.homework_set.first()
+        subscription = Subscription.objects.get(
+            user=self.request.user,
+            course=Course.objects.get(chapters=lesson.chapter),
+        )
+        reviewer = subscription.cohort.teacher
+        if homework:
             try:
-                homework = Homework.objects.get(id=request.data.get("id"))
-                homework.text = request.data.get("text")
-                return Response(request.data, status=status.HTTP_201_CREATED)
+                serializer = self.get_serializer(homework, data=request.data, partial=True)
+                serializer.is_valid(raise_exception=True)
+                serializer.save(lesson=lesson, status=Homework.ProgressionStatus.SUBMITTED, reviewer=reviewer)
+                headers = self.get_success_headers(serializer.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
             except ObjectDoesNotExist as e:
                 HomeworkException.default_code = e
                 raise HomeworkException
-
-        lesson_id = self.kwargs.get("lesson_id")
-        request.data["status"] = Homework.ProgressionStatus.SUBMITTED
-        request.data["subscription"] = Subscription.objects.get(
-            user=self.request.user,
-            course=Course.objects.get(chapters=Lesson.objects.get(id=self.kwargs.get("lesson_id")).chapter),
-        ).pk
-        request.data["lesson"] = Lesson.objects.get(id=lesson_id).pk
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save(
+            lesson=lesson, status=Homework.ProgressionStatus.SUBMITTED, subscription=subscription, reviewer=reviewer
+        )
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def perform_create(self, serializer):
-        serializer.save(
-            status=Homework.ProgressionStatus.SUBMITTED,
-        )
