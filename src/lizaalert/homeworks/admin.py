e244fd0478c3bd322ee49a0be334e19e7118b455
Pavel Melnikov <room.job@mail.ru>
2024-02-13 20:57:50 +0300

    Create admin homework

--- src/lizaalert/homeworks/admin.py
+++ src/lizaalert/homeworks/admin.py
@@ -1,1 +1,22 @@
-pass
+from django.contrib import admin
+
+from lizaalert.homeworks.models import Homework
+
+
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+    """Админка домашних заданий."""
+
+    model = (Homework,)
+    list_display = (
+        "reviewer",
+        "user",
+        "status",
+        "lesson",
+    )
+    ordering = ("lesson",)
+    list_select_related = ("lesson",)
+
+    @admin.display(empty_value="")
+    def user(self, obj):
+        return obj.subscription.user
