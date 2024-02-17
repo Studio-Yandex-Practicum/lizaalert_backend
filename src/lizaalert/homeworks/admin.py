53c0e123735e2cde70f7093803f9e84193aaef5d
mr-shifty <room.job@mail.ru>
2024-02-17 20:18:09 +0300

    Update permissions and etc.

--- src/lizaalert/homeworks/admin.py
+++ src/lizaalert/homeworks/admin.py
@@ -18,3 +18,22 @@ class HomeworkAdmin(admin.ModelAdmin):
 
     def user(self, obj):
         return obj.subscription.user
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(reviewer=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if obj is not None and not request.user.is_superuser:
+            return obj.reviewer == request.user
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.reviewer = request.user
+        super().save_model(request, obj, form, change)
