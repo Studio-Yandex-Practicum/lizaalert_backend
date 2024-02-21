0830488ec4bdb311492f4dd1a93ca627b14d7d8c
mr-shifty <room.job@mail.ru>
2024-02-21 21:03:37 +0300

    Rebase permissions.

--- src/lizaalert/homeworks/admin.py
+++ src/lizaalert/homeworks/admin.py
@@ -1,8 +1,10 @@
 from django import forms
 from django.contrib import admin
+from rest_framework.decorators import permission_classes
 from tinymce.widgets import TinyMCE
 
 from lizaalert.homeworks.models import Homework
+from lizaalert.homeworks.permissions import IsAuthenticated
 
 
 class HomeworkAdminForm(forms.ModelForm):
@@ -45,18 +47,12 @@ class HomeworkAdmin(admin.ModelAdmin):
             return qs
         return qs.filter(reviewer=request.user)
 
+    @permission_classes([IsAuthenticated])
     def has_change_permission(self, request, obj=None):
         """Проверить разрешение на изменение."""
-        if obj and not request.user.is_superuser:
-            return obj.reviewer == request.user
-        return super().has_change_permission(request, obj)
+        pass
 
+    @permission_classes([IsAuthenticated])
     def has_delete_permission(self, request, obj=None):
         """Проверить разрешение на удаление."""
-        return False
-
-    def save_model(self, request, obj, form, change):
-        """Сохранить модель."""
-        if not change:
-            obj.reviewer = request.user
-        super().save_model(request, obj, form, change)
+        pass
