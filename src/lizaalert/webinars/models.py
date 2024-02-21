184b61a4e717764eb0776459bba9a93b52b63d40
cosmofactory <nikssor@yandex.ru>
2024-02-21 21:00:08 +0100

    leave one model - Webinar

--- src/lizaalert/webinars/models.py
+++ src/lizaalert/webinars/models.py
@@ -1,15 +1,9 @@
 from django.db import models
 
 from lizaalert.courses.mixins import TimeStampedModel
-from lizaalert.homeworks.models import ProgressionStatus
-
-WEBINAR_PROGRESS = (
-    (ProgressionStatus.DRAFT, "Draft"),
-    (ProgressionStatus.APPROVED, "APPROVED"),
-)
 
 
-class WebinarTemplate(TimeStampedModel):
+class Webinar(TimeStampedModel):
     """
     Модель для создания вебинара.
 
@@ -33,33 +27,3 @@ class WebinarTemplate(TimeStampedModel):
 
     def __str__(self):
         return f"Вебинар для урока {self.lesson}"
-
-
-class Webinar(TimeStampedModel):
-    """
-    Модель прогресса вебинара.
-
-    status - прогресс вебинара;
-    subscription - подписка, к которой привязан вебинар;
-    lesson - урок, к которому привязан вебинар;
-    template - шаблон вебинара.
-    """
-
-    status = models.PositiveSmallIntegerField(
-        verbose_name="Статус",
-        choices=WEBINAR_PROGRESS,
-        default=ProgressionStatus.DRAFT,
-    )
-    subscription = models.ForeignKey("courses.Subscription", on_delete=models.CASCADE, verbose_name="Подписка")
-    template = models.ForeignKey(WebinarTemplate, on_delete=models.CASCADE, verbose_name="Шаблон вебинара")
-    lesson = models.ForeignKey(
-        "courses.Lesson", on_delete=models.CASCADE, verbose_name="Урок", related_name="webinar_progress"
-    )
-
-    class Meta:
-        verbose_name = "Прогресс вебинара"
-        verbose_name_plural = "Прогресс вебинара"
-        ordering = ["-updated_at"]
-
-    def __str__(self):
-        return f"Прогресс вебинара {self.template} для {self.subscription}"
