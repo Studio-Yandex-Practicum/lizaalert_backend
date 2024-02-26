3aba3b0fe3c6feb687e9c25779842f3ffc4c4b3d
cosmofactory <nikssor@yandex.ru>
2024-02-26 17:35:26 +0100

    add progress status to webinar, add recording_link

--- src/lizaalert/webinars/models.py
+++ src/lizaalert/webinars/models.py
@@ -12,13 +12,21 @@ class Webinar(TimeStampedModel):
     когорта - когорта, к которой привязан вебинар.
     """
 
+    class Status(models.IntegerChoices):
+        COMING = 0, "Запланирован"
+        FINISHED = 1, "Завершен"
+
     lesson = models.ForeignKey("courses.Lesson", on_delete=models.CASCADE, verbose_name="Урок", related_name="webinar")
     description = models.TextField(verbose_name="Описание вебинара", max_length=5000, blank=True, null=True)
-    link = models.URLField(verbose_name="Ссылка на вебинар", max_length=400)
+    link = models.URLField(verbose_name="Ссылка на вебинар", max_length=400, blank=True, null=True)
+    recording_link = models.URLField(verbose_name="Запись вебинара", max_length=400, blank=True, null=True)
     cohort = models.ForeignKey(
         "courses.Cohort", on_delete=models.CASCADE, verbose_name="Когорта", related_name="webinar"
     )
     webinar_date = models.DateTimeField(verbose_name="Дата вебинара")
+    status = models.PositiveSmallIntegerField(
+        verbose_name="Статус вебинара", default=Status.COMING, choices=Status.choices
+    )
 
     class Meta:
         verbose_name = "Вебинар для когорты"
--- src/lizaalert/webinars/serializers.py
+++ src/lizaalert/webinars/serializers.py
@@ -1,10 +1,15 @@
+from django.utils import timezone
+from drf_yasg.utils import swagger_serializer_method
 from rest_framework import serializers
 
+from lizaalert.settings.constants import DEFAULT_WEBINAR_LENGTH
 from lizaalert.webinars.models import Webinar
 
 
 class WebinarSerializer(serializers.ModelSerializer):
     """Сериализатор для вебинара."""
+
+    status = serializers.SerializerMethodField()
 
     class Meta:
         model = Webinar
@@ -13,9 +18,23 @@ class WebinarSerializer(serializers.ModelSerializer):
             "lesson",
             "description",
             "link",
+            "recording_link",
             "cohort",
             "webinar_date",
+            "status",
         )
+
+    @swagger_serializer_method(serializer_or_field=serializers.ChoiceField(choices=Webinar.Status.choices))
+    def get_status(self, obj):
+        """
+        Получение статуса прохождения вебинара.
+
+        Если дата-время вебинара + предполагаемая длительность вебинара в минутах, меньше текущего времени,
+        то вебинар считается завершенным, иначе - запланированным.
+        """
+        if (obj.webinar_date + timezone.timedelta(minutes=DEFAULT_WEBINAR_LENGTH)) <= timezone.now():
+            return Webinar.Status.FINISHED
+        return Webinar.Status.COMING
 
 
 class ErrorSerializer(serializers.Serializer):
