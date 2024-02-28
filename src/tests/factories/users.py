f1b5e2b099a310f0c3d510f4b6c71ed918e983bd
Vladislav-76 <sillmaril2@gmail.com>
2024-02-28 13:34:08 +0000

    TM-51 fix: user email field

--- src/tests/factories/users.py
+++ src/tests/factories/users.py
@@ -15,12 +15,12 @@ class UserFactory(factory.django.DjangoModelFactory):
         """Factory configuration."""
 
         model = User
-        django_get_or_create = ("username",)
+        django_get_or_create = ("username", )
 
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
     username = factory.Faker("user_name")
-    email = factory.Faker("email")
+    email = factory.Sequence(lambda n: f"user{n}@example.com")
     password = factory.Faker("sha256")
 
 
