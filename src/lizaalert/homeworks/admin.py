8aaf941b3467dfedcd5dcb0c402ee15a35b37727
cosmofactory <nikssor@yandex.ru>
2024-03-01 15:31:28 +0100

    add homework creation lesson restriction

--- src/lizaalert/homeworks/admin.py
+++ src/lizaalert/homeworks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from lizaalert.courses.models import Lesson
 from lizaalert.homeworks.models import Homework
 
 
@@ -18,3 +19,9 @@ class HomeworkAdmin(admin.ModelAdmin):
 
     def user(self, obj):
         return obj.subscription.user
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        """Ограничить в выдаче только уроки типа домашнее задание."""
+        if db_field.name == "lesson":
+            kwargs["queryset"] = Lesson.objects.filter(lesson_type=Lesson.LessonType.HOMEWORK)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
