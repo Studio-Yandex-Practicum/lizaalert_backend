a3b54730dce7046881228e93cc54f85c73123d7c
Your Name <you@example.com>
2024-03-01 15:44:46 +0700

    Добавил фильтр по направлению в CourseFilter

--- src/lizaalert/courses/filters.py
+++ src/lizaalert/courses/filters.py
@@ -8,16 +8,19 @@ class CourseFilter(filters.FilterSet):
     """
     Кастомный фильтр для модели Course.
 
-    Фильтрация по формату курса и ID уровня.
+    Фильтрация по формату курса, ID уровня и по направлению.
     """
 
     course_format = django_filters.CharFilter(field_name="course_format", lookup_expr="icontains")
 
     level = django_filters.BaseInFilter(field_name="level_id", lookup_expr="in")
 
+    direction = django_filters.CharFilter(field_name="direction", lookup_expr="icontains")
+
     class Meta:
         model = Course
         fields = (
             "level",
             "course_format",
+            "direction",
         )
