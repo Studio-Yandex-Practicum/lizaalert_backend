a59ceb86d2ccde3d2a0fcc1b607b99cdd97ab912
Pavel <118600823+mr-shifty@users.noreply.github.com>
2024-03-11 13:02:26 +0300

    MIL-80 Второй этап админки (#368)
    * Update permissions and etc.
    
    * Update after review.
    
    * Update after review.
    
    * add TinyMCE text redactor
    
    * Rebase permissions.
    
    * remove spellchecker
    
    * Fix after review.
    
    * Fix after review.
    
    * Fix after review.
    
    * Update lock file.
    
    * remove permissions
    
    * remove tiny mce urk
    
    ---------
    
    Co-authored-by: cosmofactory <nikssor@yandex.ru>
    Co-authored-by: Nikita Assorov <115094006+cosmofactory@users.noreply.github.com>

--- src/lizaalert/homeworks/admin.py
+++ src/lizaalert/homeworks/admin.py
@@ -1,13 +1,25 @@
+from django import forms
 from django.contrib import admin
+from tinymce.widgets import TinyMCE
 
 from lizaalert.courses.models import Lesson
 from lizaalert.homeworks.models import Homework
+
+
+class HomeworkAdminForm(forms.ModelForm):
+    class Meta:
+        model = Homework
+        fields = ("reviewer", "status", "lesson", "text", "subscription", "required")
+        widgets = {
+            "text": TinyMCE(attrs={"cols": 80, "rows": 10}),
+        }
 
 
 @admin.register(Homework)
 class HomeworkAdmin(admin.ModelAdmin):
     """Админка домашних заданий."""
 
+    form = HomeworkAdminForm
     list_display = (
         "id",
         "reviewer",
@@ -16,10 +28,24 @@ class HomeworkAdmin(admin.ModelAdmin):
         "lesson",
     )
     ordering = ("updated_at",)
-    list_select_related = ("subscription",)
+    list_select_related = ("subscription__user",)
 
     def user(self, obj):
+        """Получить пользователя, связанного с подпиской на задание."""
         return obj.subscription.user
+
+    def get_queryset(self, request):
+        """
+        Получить запрос к базе данных.
+
+        Если пользователь имеет статус суперпользователя, то он видит все домашние задания.
+        Если пользователь обычный администратор, то он видит только те домашние задания,
+         ревьюером которых является.
+        """
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(reviewer=request.user)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         """Ограничить в выдаче только уроки типа домашнее задание."""
